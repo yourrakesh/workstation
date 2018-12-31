afc5476ca321bf143d63017d1ba57ca15c43854d
yourrakesh <yourrakesh.cse.gmail.com>
2019-01-01 01:19:22 +0530

    added node live information to setup.rb

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -10,7 +10,13 @@ package 'ntp' do
 end
 
 file '/etc/motd' do
- content 'This server (VM) is the property of Rakesh.'
+ content "This server (VM) is the property of Rakesh.
+ HOSTNAME: #{node['hostname']}
+ IPADDRESS: #{node['ipaddress']}
+ CPU: #{node['cpu']['0']['mhz']}
+ MEMORY: #{node['memory']['total']}
+
+"
  action :create
  owner 'root'
  group 'root'
